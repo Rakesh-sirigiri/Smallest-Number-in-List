861d13d2ec369da37e6d0b7f4ffc3a7470dcf4f5
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-07-05 20:23:34 +0530

    Add files via upload

---
+++ Smallest Number in list.py
@@ -0,0 +1,7 @@
+list = []
+num = int(input("enter elements in a list"))
+for i in range(1,num+1):
+      ele=int(input("enter elements:"))
+      list.append(ele)
+print("Smallest element is:", min(list))
+
